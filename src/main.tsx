ef195d98489296d9a41d72c8be364c7926e14f9e
Niklas <niklas_edelstam@hotmail.com>
2023-06-08 15:12:51 +0200

    Configure React Query Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// ReactQueryDevtools is only for development mode, it will not be included in production
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
@@ -12,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
