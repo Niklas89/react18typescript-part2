ff513b93cde4c56d92045e98814e7efcc5234e40
Niklas <niklas_edelstam@hotmail.com>
2023-06-08 16:57:55 +0200

    Parameterized queries

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -8,14 +8,18 @@ interface Post {
     userId: number;
   }
 
-const usePosts = () => {
+const usePosts = (userId: number | undefined) => {
     // Benefits of using React Query below:
   // - Auto retries: if the call to the back-end fails React will retry a couple more times
   // - Automatic refetch: we can configure this query to auto refetch after a period of time
   // - Caching: the first time we fetch the data, it's stored in the cache and we can use it in the app without calling the back-end, the next time we need this data
     const fetchPosts = () =>
     axios
-      .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+            userId
+        }
+      })
       .then((res) => res.data);
 
       return useQuery<Post[], Error>({
@@ -23,7 +27,7 @@ const usePosts = () => {
         // Anytime we retrieve data from the backend,
         // it will be stored in the cache and will be accessible via this key
         // it can store multiple values, like: queryKey: ["posts", "completed", { completed: true }]
-        queryKey: ["posts"],
+        queryKey: userId ? ["users", userId, "posts"] : ["posts"], // same pattern used as when fetching data from an api, you'd use: "users/1/posts" pattern
         // queryFn: used to fetch the data from the backend
         queryFn: fetchPosts,
         staleTime: 1 * 60 * 1000 // 1min until stale : either configure staleTime individually for every request or globally in the main.tsx file
